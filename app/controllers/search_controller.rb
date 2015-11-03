87ec7f31eb9b99087a9e90597719fd7cf86950a2
benstew <benstewart2008@gmail.com>
2015-11-03 17:27:56 -0500

    Fixed search controller method to identify if raining

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,21 +6,38 @@ class SearchController < ApplicationController
     @location = params[:location]
     weather = Weather.new(@location)
     @condition = weather.is_raining?
-    case weather.temperature
-      when 'super hot'
-        @outfit = Outfit.super_hot_outfit(current_user)
-      when "hot"
-        @outfit = Outfit.hot_outfit(current_user)
-      when 'warm'
-        @outfit = Outfit.warm_outfit(current_user)
-      when 'cool'
-        @outfit = Outfit.cool_outfit(current_user)
-      when 'cold'
-        @outfit = Outfit.cold_outfit(current_user)
-      else
-        @outfit = Outfit.freezing_outfit(current_user)
+    
+    if !@condition 
+      case weather.temperature
+        when 'super hot'
+            @outfit = Outfit.super_hot_outfit(current_user)
+        when "hot"
+          @outfit = Outfit.hot_outfit(current_user)
+        when 'warm'
+          @outfit = Outfit.warm_outfit(current_user)
+        when 'cool'
+          @outfit = Outfit.cool_outfit(current_user)
+        when 'cold'
+          @outfit = Outfit.cold_outfit(current_user)
+        else
+          @outfit = Outfit.freezing_outfit(current_user)
       end
-  end
-
+    else 
+      case weather.temperature
+        when 'super hot'
+            @outfit = Outfit.super_hot_rain_outfit(current_user)
+        when "hot"
+          @outfit = Outfit.hot_rain_outfit(current_user)
+        when 'warm'
+          @outfit = Outfit.warm_rain_outfit(current_user)
+        when 'cool'
+          @outfit = Outfit.cool_rain_outfit(current_user)
+        when 'cold'
+          @outfit = Outfit.cold_rain_outfit(current_user)
+        else
+          @outfit = Outfit.freezing_rain_outfit(current_user)
+        end
+      end
+    end
 end
 
